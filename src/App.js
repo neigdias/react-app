e6b1adbdb39a9e15e8ff5459dd04b99641669b61
Claudinei Dias <neigdias@gmail.com>
2018-02-20 15:15:15 -0300

    Added style to person component and a test handler

--- src/App.js
+++ src/App.js
@@ -3,11 +3,40 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Nei", age: "32"},
+      {name: "Ana", age: "28"},
+      {name: "Carla", age: "17"}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("teste");
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: "Nei", age: "32"},
+        {name: event.target.value, age: "28"},
+        {name: "Carla", age: "17"}
+        ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app :)</h1>
-        <Person name="Nei" age="32"/>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name}
+                age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name}
+                age={this.state.persons[1].age}
+                changed={this.nameChangeHandler}/>
+        <Person name={this.state.persons[2].name}
+                age={this.state.persons[2].age}/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <h1>{props.name}</h1>
             <p>Age: {props.age}</p>
+            <input type="text" onChange={props.changed}/>
         </div>
     );
 }
