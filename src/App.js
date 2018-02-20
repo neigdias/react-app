d86ef0b588f43cfa0e5400f560211c2684eca804
Claudinei Dias <neigdias@gmail.com>
2018-02-20 15:36:15 -0300

    Added handler to show or hide people

--- src/App.js
+++ src/App.js
@@ -21,22 +21,32 @@ class App extends Component {
         {name: "Nei", age: "32"},
         {name: event.target.value, age: "28"},
         {name: "Carla", age: "17"}
-        ]
+        ],
+      showPeople: false
     })
+  }
+
+  tooglePeopleHandler = () => {
+    const doesShow = this.state.showPeople;
+    this.setState({showPeople: !doesShow});
   }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app :)</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name}
-                age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                changed={this.nameChangeHandler}/>
-        <Person name={this.state.persons[2].name}
-                age={this.state.persons[2].age}/>
+        <button onClick={this.tooglePeopleHandler}>Switch name</button>
+        { this.state.showPeople === true?
+          <div>
+            <Person name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}/>
+            <Person name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    changed={this.nameChangeHandler}/>
+            <Person name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
+          </div> : null
+        }
       </div>
     );
   }
