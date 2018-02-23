2a50e47613ca416d558e20ebb8b6170f84db4c2b
Claudinei Dias <neigdias@gmail.com>
2018-02-23 15:47:53 -0300

    Added @mediaType on person component using radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 
 const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)' :{
+            width: '450px'
+        }
+    }
     return (
-        <div className="Person">
+        <div className="Person" style={style} >
             <div className="PersonClose" onClick={props.click}>X</div>
             <h1>{props.name}</h1>
             <p>Age: {props.age}</p>
@@ -11,4 +17,4 @@ const person = (props) => {
         </div>
     );
 }
-export default person;
+export default Radium(person);
